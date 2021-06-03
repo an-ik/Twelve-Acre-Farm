f1813f45d9c301c4ba351960f8aff6b9807122cb
Andrew Dominik <adominik@massmutual.com>
2021-06-03 12:27:15 -0400

    Update App.js

--- App.js
+++ App.js
@@ -11,6 +11,6 @@ router.get('/', function (req, res) {
 //add the router
 app.use('/', router);
 app.use(express.static(__dirname + '/public'));
-app.listen(process.env.port || 3000);
+// app.listen(process.env.port || 3000);
 
 console.log('Running at Port 3000');
